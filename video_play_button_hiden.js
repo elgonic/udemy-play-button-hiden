5db3ed230506080cf4a8e40a27079f11cf99ffc8
ssrs <ryo.227069@gmail.com>
2022-01-03 00:11:14 +0900

    play-button名称変更

--- video_play_button_hiden.js
+++ video_play_button_hiden.js
@@ -2,9 +2,9 @@ window.addEventListener("load", main, false);
 function main(e){
     const jsInitCheckTimer=setInterval(jsLoaded,1000);
     function jsLoaded(){
-        if(document.querySelector('.video-player--center-button--aPLkI') != null){
+        if(document.querySelector("[class^='video-player--center']") != null){
             clearInterval(jsInitCheckTimer);
-            $('div[class*="video-player--center-button-"]').hide();
+            $('div[class*="video-player--center-"]').hide();
         }
     }
 }
