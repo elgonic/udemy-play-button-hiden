bbd4450319e9b48897ba286e18a53762cc6925e0
ssrs <ryo.227069@gmail.com>
2022-01-05 23:54:41 +0900

    修正

--- video_play_button_hiden.js
+++ video_play_button_hiden.js
@@ -1,11 +1,21 @@
 window.addEventListener("load", main, false);
-function main(e){
+function main(){
     const jsInitCheckTimer=setInterval(jsLoaded,1000);
+    jsLoaded_kye;
+
     function jsLoaded(){
         if(document.querySelector("[class^='video-player--center']") != null){
             clearInterval(jsInitCheckTimer);
             $('div[class*="video-player--center-"]').hide();
         }
+    }
+
+    function jsLoaded_kye(){
+        document.addEventListener("keydown",event => {
+            if(event.code =="Enter"){
+                $('div[class*="video-player--center-"]').hide();
+            }
+        })
     }
 }
 
