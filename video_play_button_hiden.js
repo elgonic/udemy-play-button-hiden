f4048b504a8e8a0a251a9e0f99746c7c25e88110
ssrs <ryo.227069@gmail.com>
2022-01-08 22:05:31 +0900

    動画画面の変更検知

--- video_play_button_hiden.js
+++ video_play_button_hiden.js
@@ -1,8 +1,39 @@
-//window.addEventListener('load', main, true);
-const jsInitCheckTimer=setInterval(jsLoaded,1000);
-function jsLoaded(){
+// setTimeout x秒後に処理
+// 複数回同時に走った場合、一杯いっぱいなので、こちらに
+const timer = setTimeout(getProperty, 1000);
+function getProperty() {
+    // 対象要素要件等
+    target = document.getElementsByClassName("curriculum-item-view--content--3ABmp")
+    // curriculum-item-view--video-background--lcepY 
+    // curriculum-item-view--absolute-height-limiter--1SMqE
+    // vjs-tech
+    // clearInterval(timer);
+
+    // 要件等
+    observer.observe(target[0], {
+        attributes: true,
+        attributeOldValue: false,
+        characterData: false,
+        characterDataOldValue: false,
+        childList: false,
+        subtree: false
+    });
+}
+
+// 変更検知したら処理
+const observer = new MutationObserver(function (mutations) {
+    // console.log(mutations);
+    // console.log("test");
+    console.log("検知");
+    // timer2 = setInterval(hiddenButton, 2000);
+    timer2 = setTimeout(hiddenButton, 1000);
+});
+
+// ボタンを隠す処理1
+function hiddenButton() {
+    // clearInterval(timer2);
     if(document.querySelector("[class^='video-player--center']") != null){
         $('div[class*="video-player--center-"]').hide();
-        console.log("Hide play button")
+            console.log("発火")
     }
 }
