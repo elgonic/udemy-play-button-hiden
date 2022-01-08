c39ab71c244c1d73b91a243852fb3e9d207f6ea1
ssrs <ryo.227069@gmail.com>
2022-01-08 22:00:42 +0900

    不要なコード削除

--- video_play_button_hiden.js
+++ video_play_button_hiden.js
@@ -1,13 +1,8 @@
 //window.addEventListener('load', main, true);
-const jsInitCheckTimer=setInterval(main,1000);
-function main(){
-    const jsInitCheckTimer=setInterval(jsLoaded,1);
-    function jsLoaded(){
-        if(document.querySelector("[class^='video-player--center']") != null){
-            clearInterval(jsInitCheckTimer);
-            $('div[class*="video-player--center-"]').hide();
-            //console.log("発火")
-        }
-
+const jsInitCheckTimer=setInterval(jsLoaded,1000);
+function jsLoaded(){
+    if(document.querySelector("[class^='video-player--center']") != null){
+        $('div[class*="video-player--center-"]').hide();
+        console.log("Hide play button")
     }
 }
